2cb6b857ca13070ad2adf359768bbefa2245b308
Ryan Moultrup <rmoultrup@insiderealestate.com>
2021-06-29 23:50:18 -0400

    First commit on Vue 3

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'roulette': ['"Libre Baskerville"', 'serif']
+      },
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+        '14': 'repeat(14, minmax(0, 1fr))'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
