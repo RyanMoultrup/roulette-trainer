3562f0bbaf78fcd5bcaae74bfe470153f28fe67e
Ryan Moultrup <rmoultrup@gmail.com>
2023-07-08 13:28:45 -0500

    Refactors game.js more to remove if/else statements

--- src/lib/game.js
+++ src/lib/game.js
@@ -1,51 +1,56 @@
 import spots from '@/lib/table/spots';
 import store from '@/store/index';
 
+const buildWinObject = (...[placement, hit, winnings, bank, betAmt]) => {
+  return {
+    placement: placement,
+    wonRound: 1,
+    lostRound: 0,
+    won: winnings,
+    loss: 0,
+    bet: betAmt,
+    hit: hit,
+    color: spots[hit].color,
+    even: hit % 2 === 0,
+    bank: bank,
+    round: store.state.simulation.rounds + 1,
+    outcome: 'Won'
+  }
+};
+
+const buildLostObject = (...[placement, hit, , bank, betAmt]) => {
+  return {
+    placement: placement,
+    wonRound: 0,
+    lostRound: 1,
+    won: 0,
+    loss: betAmt,
+    bet: betAmt,
+    hit: hit,
+    color: spots[hit].color,
+    even: hit % 2 === 0,
+    bank: bank,
+    round: store.state.simulation.rounds + 1,
+    outcome: 'Lost'
+  }
+}
+
 const game = {
   play (hit) {
     const myBets = store.getters["strategy/getStrategy"];
-    const outcomes = [];
     let bank = store.getters['bank/balance'];
 
-    myBets.forEach(bet => {
-      let winnings;
-      let betAmt = bet.get();
+    const outcomes = myBets.map(bet => {
+      const betAmt = bet.get();
+      const winnings = bet.collect(hit);
 
-      winnings = bet.collect(hit);
+      bank = winnings ? bank + (winnings - betAmt) : bank - betAmt;
 
-      if (winnings) {
-        bank = bank + (winnings - betAmt);
-        outcomes.push({
-          placement: bet.placement(),
-          wonRound: 1,
-          lostRound: 0,
-          won: winnings - betAmt,
-          loss: 0,
-          bet: betAmt,
-          hit: hit,
-          color: spots[hit].color,
-          even: hit % 2 === 0,
-          bank: bank,
-          round: store.state.simulation.rounds + 1,
-          outcome: 'Won'
-        });
-      } else {
-        bank = bank - betAmt;
-        outcomes.push({
-          placement: bet.placement(),
-          wonRound: 0,
-          lostRound: 1,
-          won: 0,
-          loss: betAmt,
-          bet: betAmt,
-          hit: hit,
-          color: spots[hit].color,
-          even: hit % 2 === 0,
-          bank: bank,
-          round: store.state.simulation.rounds + 1,
-          outcome: 'Lost'
-        });
-      }
+      const args = [bet.placement(), hit, winnings - betAmt, bank, betAmt]
+
+      return winnings
+          ? buildWinObject(...args)
+          : buildLostObject(...args);
     });
 
     store.commit('bank/updateBank', bank);
