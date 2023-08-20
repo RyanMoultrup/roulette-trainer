ae948c2dd8b471f668d5fd4df8f281d78195cca4
Ryan Moultrup <rmoultrup@gmail.com>
2023-08-19 20:52:11 -0500

    Refactors table to use an object rather than plain HTML

---
+++ src/lib/table/table.js
@@ -0,0 +1,204 @@
+export const tableSpots = [
+    {
+        class: 'spot _0 pt-16 cell border-t-3 outside-cell align-text-middle row-span-3 rounded-tl rounded-bl left-border-cell bottom-border-cell top-border-cell',
+        text: '0'
+    },
+    {
+        class: 'spot cell left-border-cell red-cell top-border-cell',
+        text: '3'
+    },
+    {
+        class: 'spot cell black-cell  top-border-cell',
+        text: '6'
+    },
+    {
+        class: 'spot cell red-cell top-border-cell',
+        text: '9'
+    },
+    {
+        class: 'spot cell red-cell top-border-cell',
+        text: '12'
+    },
+    {
+        class: 'spot cell black-cell top-border-cell',
+        text: '15'
+    },
+    {
+        class: 'spot cell red-cell top-border-cell',
+        text: '18'
+    },
+    {
+        class: 'spot cell red-cell top-border-cell',
+        text: '21'
+    },
+    {
+        class: 'spot cell black-cell top-border-cell',
+        text: '24'
+    },
+    {
+        class: 'spot cell red-cell top-border-cell',
+        text: '27'
+    },
+    {
+        class: 'spot cell red-cell top-border-cell',
+        text: '30'
+    },
+    {
+        class: 'spot cell black-cell top-border-cell',
+        text: '33'
+    },
+    {
+        class: 'spot cell cell right-border-cell border-t-3 red-cell top-border-cell',
+        text: '36'
+    },
+    {
+        class: 'spot cell outside-cell text-base rounded-tr rounded-br top-border-cell right-border-cell',
+        text: '2/1'
+    },
+
+    {
+        class: 'spot cell cell left-border-cell black-cell',
+        text: '2'
+    },
+    {
+        class: 'spot cell red-cell',
+        text: '5'
+    },
+    {
+        class: 'spot cell black-cell',
+        text: '8'
+    },
+    {
+        class: 'spot cell black-cell',
+        text: '11'
+    },
+    {
+        class: 'spot cell red-cell',
+        text: '14'
+    },
+    {
+        class: 'spot cell black-cell',
+        text: '17'
+    },
+    {
+        class: 'spot cell black-cell',
+        text: '20'
+    },
+    {
+        class: 'spot cell red-cell',
+        text: '23'
+    },
+    {
+        class: 'spot cell black-cell',
+        text: '26'
+    },
+    {
+        class: 'spot cell black-cell',
+        text: '29'
+    },
+    {
+        class: 'spot cell red-cell',
+        text: '32'
+    },
+    {
+        class: 'spot cell right-border-cell black-cell',
+        text: '35'
+    },
+    {
+        class: 'spot cell outside-cell text-base right-border-cell rounded-br rounded-tr',
+        text: '2/1'
+    },
+
+    {
+        class: 'spot cell left-border-cell red-cell bottom-border-cell',
+        text: '1'
+    },
+    {
+        class: 'spot cell black-cell bottom-border-cell',
+        text: '4'
+    },
+    {
+        class: 'spot cell red-cell bottom-border-cell',
+        text: '7'
+    },
+    {
+        class: 'spot cell black-cell bottom-border-cell',
+        text: '10'
+    },
+    {
+        class: 'spot cell black-cell bottom-border-cell',
+        text: '13'
+    },
+    {
+        class: 'spot cell red-cell bottom-border-cell',
+        text: '16'
+    },
+    {
+        class: 'spot cell red-cell bottom-border-cell',
+        text: '19'
+    },
+    {
+        class: 'spot cell black-cell bottom-border-cell',
+        text: '22'
+    },
+    {
+        class: 'spot cell red-cell bottom-border-cell',
+        text: '25'
+    },
+    {
+        class: 'spot cell black-cell bottom-border-cell',
+        text: '28'
+    },
+    {
+        class: 'spot cell black-cell bottom-border-cell',
+        text: '31'
+    },
+    {
+        class: 'spot cell right-border-cell red-cell bottom-border-cell',
+        text: '34'
+    },
+    {
+        class: 'spot cell outside-cell text-base rounded-br rounded-tr bottom-border-cell right-border-cell',
+        text: '2/1'
+    },
+
+    {
+        class: 'spot cell outside-cell text-large col-span-4 col-start-2 left-border-cell rounded-tl',
+        text: '1ST 12'
+    },
+    {
+        class: 'spot cell outside-cell text-large col-span-4',
+        text: '2ND 12'
+    },
+    {
+        class: 'spot cell outside-cell text-large col-span-4 right-border-cell rounded-tr',
+        text: '3RD 12'
+    },
+
+    {
+        class: 'spot cell pt-4 outside-cell text-xl col-span-2 col-start-2 rounded-bl bottom-border-cell left-border-cell',
+        text: '1 TO 18'
+    },
+    {
+        class: 'spot cell outside-cell col-span-2 bottom-border-cell',
+        text: 'EVEN'
+    },
+
+    {
+        class: 'spot cell red-cell col-span-2 bottom-border-cell',
+        text: 'RED'
+    },
+    {
+        class: 'spot cell p-2 pt-4 black-cell col-span-2 bottom-border-cell',
+        text: 'BLACK'
+    },
+
+    {
+        class: 'spot cell outside-cell text-opacity-70 col-span-2 bottom-border-cell',
+        text: 'ODD'
+    },
+    {
+        class: 'spot cell p-2 pt-4 outside-cell text-xl col-span-2 rounded-br right-border-cell bottom-border-cell',
+        text: '19 TO 36'
+    },
+]
