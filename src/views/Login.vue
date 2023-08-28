5ccfeb6fd17cabd4af4e76c93474ab7e361e77d2
Ryan Moultrup <rmoultrup@gmail.com>
2023-08-28 16:27:51 -0500

    Adds feedback to login screen

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,11 @@
 <template>
   <main class="h-screen w-full bg-center bg-no-repeat bg-cover" :style="{ backgroundImage: 'url(' + bgImage + ')' }">
-
     <div class="absolute flex flex-col gap-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-10 rounded-md shadow-lg bg-gradient-to-tr from-green-700 via-green-800 to-green-800 border border-green-800 z-[200] backdrop-filter backdrop-blur-sm shadow drop-shadow-lg">
       <img  class="max-w-lg" src="@/assets/logo.png" alt="">
       <p class="text-gray-400 text-lg py-3">Login to Your Account</p>
+      <div v-show="showError" class="bg-accent-400 p-5 rounded">
+        {{ authError }}
+      </div>
       <form action="">
         <div class="mb-3">
           <span class="text-normal block font-medium text-gray-900 dark:text-gray-300 mb-2">Email</span>
@@ -28,37 +30,54 @@
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
-import BaseInput from "@/components/ui/Base/BaseInput.vue";
+import { auth } from "@/api"
+import { useStore } from "vuex";
 import bgImg from '@/assets/login-bg.png'
-import {useStore} from "vuex";
-import {useRouter} from "vue-router";
-import { auth } from "@/api";
-import {storeToken} from "@/lib/storage/auth/TokenStorage";
+import { defineComponent, ref } from "vue"
+import { useRouter, useRoute } from "vue-router"
+import BaseInput from "@/components/ui/Base/BaseInput.vue"
+import { storeToken } from "@/lib/storage/auth/TokenStorage"
 
 export default defineComponent({
   components: { BaseInput },
   setup() {
     const store = useStore()
     const router = useRouter()
+    const route = useRoute()
     const bgImage = bgImg // Adjust the path
     const formData = ref({
       email: '',
       password: ''
     })
+    const authError = ref('')
+    const showError = ref(false)
+
+    // if (route.query.demo) {
+      formData.value.email = 'demo@email.com'
+      formData.value.password = 'Password'
+    // }
 
     const submitForm = async () => {
-      const { status, data: { error, userId, token } } = await auth.login(formData.value)
-      if (status === 200 && !error) storeToken(token)
-      await store.dispatch('user/setUser', userId)
-      await store.dispatch('user/authenticate')
-      router.push({ name: 'home' })
+      auth.login(formData.value).then(async ({ status, data }) => {
+        if (status === 200 && !data.error) storeToken(data.token)
+        await store.dispatch('user/setUser', data.userId)
+        await store.dispatch('user/authenticate')
+        router.push({ name: 'home' })
+      }).catch((err) => {
+        console.log('in the catch', err.response.status)
+        if (err.response.status === 404 || err.response.status === 401) {
+          authError.value = err.response.data.msg
+          showError.value = true
+        }
+      })
     }
 
     return {
       bgImage,
       formData,
-      submitForm
+      submitForm,
+      authError,
+      showError
     }
   }
 })
