31efeace2d62d4545a7fe0cc89255d8e0f2669c5
Ryan Moultrup <rmoultrup@gmail.com>
2023-09-15 11:48:52 -0500

    Add texst and clean layout for coverage table

--- src/components/ActiveGameCharts.vue
+++ src/components/ActiveGameCharts.vue
@@ -2,7 +2,7 @@
   <section :class="gridClass" class="bg-opacity-0 mx-4 mt-4 mb-2">
     <base-card grid-class="win-loss-bank-chart" :top-corner-gradient="true">
       <div class="relative overflow-hidden flex flex-col gap-3 h-full">
-<!--        <display-stats />-->
+        <span class="font-lobster text-2xl">Bet Coverage & Win Probability</span>
         <win-probability />
         <coverage-table />
       </div>
